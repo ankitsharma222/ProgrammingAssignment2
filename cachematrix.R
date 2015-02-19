cd47e3d97c935fdd4aa78c624fc91c4c1cf66cc4
ankitsharma222 <ankitsharma1944@gmail.com>
2015-02-20 01:47:18 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## these functions help in caching the inverse of a matrix rather than computing it repeatedly 
+##to set or get the value of matrix or its inverse
+makeCacheMatrix<-function(x=matrix()){
+  m<-NULL
+  set<-function(y=matrix()){
+    x<<-y
+    m<<-NULL
+                                   ##to assign new data to x and discard the old inverse stored in m.      
+  }
+  get<-function()x                ## to get the value of x
+  setinv<-function(inv) m<<-inv   ##takes inv as an argument and save its value in the m variable in global env
+  getinv<-function() m            ##returns the value of mean
+  
+  list(set=set,get=get,setinv=setinv,getinv=getinv)## makecachemeans returns a list of functions
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##2nd function to calculate the inverse or to get a cached value
+cacheSolve<-function(x=matrix(),...){
+  m<-z$getinv()                     ##z=makeCacheMatrix(...)
+  if(!is.null(m))  {
+    print("printing cached inverse") ##to check if m is already calculated
+    return(m)
+  }
+  mat<-z$get()
+  m<-solve(mat)                     ## to calculate inv if it ws not previously calculated
+  z$setinv(m)                       ##to set new value of inverse just calculated
+  m
 }
